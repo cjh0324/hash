9ed310c5af0fdec3639b8bb4a9ba5d1dcb377750
cho <cho@DESKTOP-9V214U5.localdomain>
2022-12-02 10:46:47 +0900

    single llc set

--- util/count.py
+++ util/count.py
@@ -1,12 +1,12 @@
 count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 limit = 0
 
-with open("mapping") as bookfile:
+with open("txt/mapping_Xeon") as bookfile:
     for line in bookfile:
         num = int(line[-3:-1])
         count[num] += 1
         limit += 1
-        if (limit == 512): 
+        if (limit == 4096): 
             break
 
 for i, count in enumerate(count):
