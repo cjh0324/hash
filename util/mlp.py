04e3d120992ba4e157dd99283ec469dd7c4b2dd1
Jaehoon Cho <jaehoon@greenstar.snu.ac.kr>
2022-12-19 15:07:42 +0900

    dodo

--- util/mlp.py
+++ util/mlp.py
@@ -14,27 +14,22 @@ import random
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 
-# In[9]:
+# In[2]:
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(28, 124)
-        self.fc2 = nn.Linear(124, 64)
-        self.fc3 = nn.Linear(64, 20)
-        self.fc4 = nn.Linear(20, 18)
-        # self.fc4 = nn.Linear(6, 4)
-        # self.fc5 = nn.Linear(4, 2)
+        self.fc1 = nn.Linear(28, 256)
+        self.fc2 = nn.Linear(256, 1024)
+        self.fc3 = nn.Linear(1024, 2048)
+        self.fc4 = nn.Linear(2048, 8192)
+        self.fc5 = nn.Linear(8192, 18)
 
     def forward(self, x):
-        # print("type x", type(x))
-        # x = self.fc1(x)
-        # x = F.relu(x)
-        # x = self.fc2(x)
-        # x = F.relu(x)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
@@ -42,10 +37,12 @@ class Net(nn.Module):
         x = self.fc3(x)
         x = F.relu(x)
         x = self.fc4(x)
+        x = F.relu(x)
+        x = self.fc5(x)
         return x
 
 
-# In[10]:
+# In[3]:
 
 
 class CustomDataset(Dataset):
@@ -53,40 +50,32 @@ class CustomDataset(Dataset):
         df = pd.read_csv(file_path)
         self.x = df.iloc[:,0].values
         self.y = df.iloc[:,1].values
-        # self.a = df.iloc[:, 0].values
-        # self.b = df.iloc[:, 1].values
-        # self.c = df.iloc[:, 2].values
-        # self.y = df.iloc[:, 3].values
-        # self.y = list(map(self.string_to_vector, self.y))
         self.length = len(df)
-
-    # def string_to_vector(self, value):
-    #     data = {"acute triangle": 0, "right triangle":1, "obtuse triangle": 2}
-    #     return data.get(value, None)
 
     def __getitem__(self, index):
         x = self.x[index]
-        # x = list(str(x))[:-6]
-        # x = list(map(int, x))
-        # while(len(x)<8):
-        #     x.insert(0, 0)
         x = list(x)[:-6]
         x = list(map(int, x))
+        # x = [1 if xi == 1 else -1 for xi in x]
         x = torch.FloatTensor(x)
-        # x = torch.FloatTensor(sorted([self.a[index], self.b[index], self.c[index]]))
         y = torch.LongTensor(self.y)[index]
-        # print(x, y)
         return x, y
 
     def __len__(self):
         return self.length
 
 
-# In[11]:
+# In[4]:
 
 
-# train_dataset = CustomDataset("txt/data2.csv")
+print(os.getcwd())
+
+
+# In[5]:
+
+
 full_dataset = CustomDataset("txt/data_Xeon.csv")
+# full_dataset = CustomDataset("C:/Users/1998b/Google 드라이브/학부/4학년 2학기/hash/txt/data_Xeon.csv")
 # full_dataset = CustomDataset("/content/drive/MyDrive/학부/4학년 2학기/hash/txt/data.csv")
 train_size = int(0.8 * len(full_dataset))
 test_size = len(full_dataset) - train_size
@@ -100,41 +89,39 @@ model = Net().to(device)
 print(model)
 
 
-# In[12]:
+# In[6]:
 
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
 
-# In[13]:
+# In[ ]:
 
 
 losses = []
 
-indices = torch.randperm(len(train_dataset))[:64768]
+indices = torch.randperm(len(train_dataset))[:2097152]
 sampler = SubsetRandomSampler(indices)
 train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False, drop_last=True, sampler=sampler)
 
-for epoch in range(10000):
+for epoch in range(200):
     cost = 0.0
 
-    # indices = torch.randperm(len(train_dataset))[:129536]
+    # indices = torch.randperm(len(train_dataset))[:131072]
     # sampler = SubsetRandomSampler(indices)
     # train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=False, drop_last=True, sampler=sampler)
 
-    for i, batch in enumerate(tqdm(train_dataloader)):
-    # for x, y in train_dataloader:
-        x = batch[0].to(device)
-        y = batch[1].to(device)
-        # x = x.to(device)
-        # y = y.to(device)
+    # for i, batch in enumerate(tqdm(train_dataloader)):
+    for x, y in train_dataloader:
+        # x = batch[0].to(device)
+        # y = batch[1].to(device)
+        x = x.to(device)
+        y = y.to(device)
         optimizer.zero_grad()
-        # print(x, y)
         
         output = model(x)
-        # print(type(output), output.shape, y.shape)
-        # print(output, y)
+
         loss = criterion(output, y)
 
         loss.backward()
@@ -145,23 +132,23 @@ for epoch in range(10000):
         # print(f"Epoch : {epoch+1:4d}, Loss : {loss:.3f}")
 
     cost = cost / len(train_dataloader)
-    losses.append(cost)
+    losses.append(cost.item())
 
-    if (epoch + 1) % 10 == 0:
-        print(f"Epoch : {epoch+1:4d}, Cost : {cost:.3f}")
-
-
-# In[ ]:
+    # if (epoch + 1) % 10 == 0:
+    print(f"Epoch : {epoch+1:4d}, Cost : {cost:.3f}")
 
 
-losses_int = [ts.item() for ts in losses]
-plt.plot(losses_int, 'r')
+# In[16]:
+
+
+plt.plot(losses, 'r')
 plt.xlabel('epoch')
 plt.ylabel('loss')
-plt.title('Intel Core i7 7500U')
+plt.title('Xeon Gold 6136')
+plt.savefig('epoch 1000.png')
 plt.show()
 
-indices = torch.randperm(len(test_dataset))[:100000]
+indices = torch.randperm(len(test_dataset))[:2000000]
 sampler = SubsetRandomSampler(indices)
 test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False, drop_last=True, sampler=sampler)
 
